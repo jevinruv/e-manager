ec4b948d372ede6fd9350792f292ce247f6eaeac
Jevin Fernando <jevin.fernando@ifsworld.com>
2020-04-02 11:44:45 +0530

    finalized for local deployment

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const request = require('request');
 
 // Serve static files....
 app.use(express.static(__dirname + '/dist/e-manager'));
+
+// app.get("/api/*", (req, res) => {
+
+//   const url = 'https://e-manager-api.herokuapp.com/api';
+//   request(url).pipe(res);
+// });
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
