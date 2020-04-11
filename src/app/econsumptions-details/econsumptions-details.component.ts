4fb36cf595592c01eb84c64a50757938cd7dc9a2
jevinruv <jevinruv@gmail.com>
2020-04-11 20:33:33 +0530

    consumptions corrections #3

--- src/app/econsumptions-details/econsumptions-details.component.ts
+++ src/app/econsumptions-details/econsumptions-details.component.ts
@@ -18,13 +18,29 @@ export class EconsumptionsDetailsComponent implements OnInit {
   isReadOnly = true;
   id: string;
   customerCategoryList: CustomerCategory[] = [];
+  eConsumptionList: EConsumption[] = [];
   selectedCustomerCategory;
+
+  graphData: any[];
+  view: any[] = [700, 300];
+
+  // options
+  legend: boolean = true;
+  showLabels: boolean = true;
+  animations: boolean = true;
+  xAxis: boolean = true;
+  yAxis: boolean = true;
+  showYAxisLabel: boolean = true;
+  showXAxisLabel: boolean = true;
+  xAxisLabel: string = 'Year';
+  yAxisLabel: string = 'Population';
+  timeline: boolean = true;
 
   constructor(
     private route: ActivatedRoute,
     private eConsumptionService: EconsumptionService,
     private customerCategoryService: CustomerCategoryService,
-    private toastr: ToastrService  ) { }
+    private toastr: ToastrService) { }
 
   ngOnInit() {
 
@@ -39,6 +55,11 @@ export class EconsumptionsDetailsComponent implements OnInit {
       this.eConsumptionService.get(this.id).subscribe(data => {
         console.log(data);
         this.eConsumption = data;
+      });
+
+      this.eConsumptionService.getAll().subscribe(data => {
+        console.log(data);
+        this.eConsumptionList = data;
       });
 
     }
@@ -88,5 +109,39 @@ export class EconsumptionsDetailsComponent implements OnInit {
     this.toastr.success("Calculation Submitted");
   }
 
+  viewGraph() {
+
+    let year = this.eConsumption.consumptionDate.toString().split("-")[0];
+
+    this.eConsumptionList = this.eConsumptionList.filter(o => o.consumptionDate.toString().split("-")[0] == year);
+    // console.log(this.eConsumptionList);
+
+    let actual = this.eConsumptionList.map(o => ({ value: o.consumptionActualCost, name: o.consumptionDate }));
+    let planned = this.eConsumptionList.map(o => ({ value: o.consumptionPlannedCost, name: o.consumptionDate }));
+
+    this.graphData = [
+      { name: "Actual", series: actual },
+      { name: "Planned", series: planned },
+    ];
+
+    console.log(this.graphData);
+
+    this.validateConsumptions();
+  }
+
+  private validateConsumptions(){
+
+    if(this.eConsumption.consumptionActualCost > this.eConsumption.consumptionPlannedCost){
+
+      let difference = this.eConsumption.consumptionActualCost - this.eConsumption.consumptionPlannedCost;
+
+      this.toastr.warning("Consumption is higher by LKR " + difference);
+    }
+    else{
+      let difference = this.eConsumption.consumptionPlannedCost - this.eConsumption.consumptionActualCost;
+
+      this.toastr.success("Consumption is Saved by LKR " + difference);
+    }
+  }
 
 }
